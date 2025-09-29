3341260d964499c6ad952b74aac7dd7d25860af8
ashitiz8697 <97868229+ashitiz8697@users.noreply.github.com>
2025-09-29 11:57:03 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,18 @@
-from datetime import datetime
+# utils.py (add dependencies: geopy, timezonefinder)
 from dateutil import parser
+from zoneinfo import ZoneInfo
+from geopy.geocoders import Nominatim
+from timezonefinder import TimezoneFinder
 
+def parse_datetime(date_str: str, time_str: str = None, place: str = None):
+    txt = f"{date_str}T{time_str}" if time_str else date_str
+    dt = parser.parse(txt)              # naive
+    if place:
+        geo = Nominatim(user_agent="astro_insight")
+        loc = geo.geocode(place, timeout=10)
+        if loc:
+            tz = TimezoneFinder().timezone_at(lat=loc.latitude, lng=loc.longitude)
+            if tz:
+                return dt.replace(tzinfo=ZoneInfo(tz))
+    return dt
 
-
-
-def parse_datetime(date_str: str, time_str: str = None) -> datetime:
-if time_str:
-txt = f"{date_str}T{time_str}"
-else:
-txt = date_str
-# dateutil parser is flexible
-dt = parser.isoparse(txt) if 'T' in txt else parser.parse(txt)
-return dt
